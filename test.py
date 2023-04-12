7a5a95f37fbb75e0b47e3d5269533b392db2e491
FreeLCZ <15327459326@163.com>
2023-04-12 10:50:04 +0800

    [add]
    1、code review test

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+from PIL import Image
+
+path = "E:/study/chat-gpt/example1/"
+for file in os.listdir(path):
+    if file.endswith(".bmp"):
+        img = Image.open(os.path.join(path, file))
+        img.show()
